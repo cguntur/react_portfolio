527e979f7b09118678485337fa86a048093aab9d
Chandrika Sista <cguntur@gmail.com>
2024-06-19 14:11:52 -0700

    Updated the pages and the components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,11 @@ import Nav from './NavTabs';
 
 function Header() {
     //const currentPage = useLocation().pathname;
-
     return (
         <div className="header">
             <h1 className="title">My Portfolio</h1>
             <Nav />
         </div>
-    
     )
-    
 }  
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
