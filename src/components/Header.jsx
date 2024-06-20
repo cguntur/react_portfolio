47132112adae90dbaeb530ca9428dcf589698f8f
Chandrika Sista <cguntur@gmail.com>
2024-06-19 18:25:06 -0700

    Added the navbar to the header with some custom styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 //import { Link, useLocation } from 'react-router-dom';
 
-import Nav from './NavTabs';
+//import Nav from './NavTabs';
+import NavList from './NavList';
 
 function Header() {
     //const currentPage = useLocation().pathname;
     return (
         <div className="header">
-            <h1 className="title">My Portfolio</h1>
-            <Nav />
+            <h1 className="title"><a href="/">My Portfolio</a></h1>
+            <NavList />
         </div>
     )
 }  
--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,56 +1,71 @@
-import { Link, useLocation } from 'react-router-dom';
+//import { useLocation } from 'react-router-dom';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+
+import "../assets/css/Navbar.css";
+
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function NavTabs() {
-  const currentPage = useLocation().pathname;
 
-  return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/"
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/about"
-          className={currentPage === '/about' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/resume"
-          className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/portfolio"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
-  );
+const NavTabs = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  //const currentPage = useLocation().pathname;
+
+ // const menuItems = ["Home", "About", "Portfolio", "Resume", "Contact"];
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const closeMenuOnMobile = () => {
+        if (window.innerWidth <= 1150) {
+          setShowMenu(false);
+        }
+    };
+
+    return (
+      <nav className="nav container">
+        <div className={`nav__menu ${showMenu ? "show-menu" : ""}`} id="nav-menu">
+          <ul className="nav nav__list">
+            <li className="nav__item">
+                <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/About" className="nav__link" onClick={closeMenuOnMobile}>
+                  About
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/Portfolio" className="nav__link" onClick={closeMenuOnMobile}>
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/Resume" className="nav__link" onClick={closeMenuOnMobile}>
+                  Resume
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/Contact" className="nav__link" onClick={closeMenuOnMobile}>
+                  Contact
+                </NavLink>
+              </li>
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+              <IoClose />
+          </div>
+          </div>
+   
+          <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+            <IoMenu />
+          </div>  
+      </nav>
+      
+    );
 }
 
 export default NavTabs;
