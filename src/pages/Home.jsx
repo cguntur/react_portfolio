ff288acdfd7b4b358a6d401e7a8498d9794c0533
Chandrika Sista <cguntur@gmail.com>
2024-06-25 19:28:13 -0700

    Updated the links on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
           <p>
             Hi! Welcome. My name is Chandrika Sista and I am a full stack web developer.
           </p>
-          <p>Look through my <a href="/portfolio">portfolio</a> to see the projects I&aposve worked on. </p>
-          <p>If you like my pojects and would want something similar for your website, please leave me a note <a href="/contact">here</a>.</p>
+          <p>Look through my <a href="/Portfolio">portfolio</a> to see the projects I&aposve worked on. </p>
+          <p>If you like my pojects and would want something similar for your website, please leave me a note <a href="/Contact">here</a>.</p>
         </div>
       );
 }
